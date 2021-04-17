cef4d72488293defafb0132d2749b23bb0a04d20
Javier Alejandro <javiagl98@gmail.com>
2021-04-17 12:22:41 -0500

    Pendiente Pedidos

--- Backend/main.py
+++ Backend/main.py
@@ -54,6 +54,14 @@ def loginmetodo():
 
 
 
+# ========= RUTA Pendite =========
+@app.route('/PedidosNuevos', methods=['POST'])
+def PedidosPruebas():
+        data = request.get_json(force=True)
+        return jsonify("En prueba")
+
+
+
 
 if __name__ == "__main__":
     app.run("0.0.0.0",port=5050)
